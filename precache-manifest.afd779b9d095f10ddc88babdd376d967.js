ba167bd3becd958c9a812e2e6d7be05654f1ce3d
Philippe <philippe2008@hotmail.com>
2018-10-10 00:55:58 -0300

    Updates

---
+++ precache-manifest.afd779b9d095f10ddc88babdd376d967.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testdeploy/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "af77c1c4c87271addcd3",
+    "url": "/testdeploy/static/js/runtime~main.af77c1c4.js"
+  },
+  {
+    "revision": "83cebac5d5b3cc836d00",
+    "url": "/testdeploy/static/js/main.83cebac5.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/testdeploy/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "83cebac5d5b3cc836d00",
+    "url": "/testdeploy/static/css/main.f3bfa98e.chunk.css"
+  },
+  {
+    "revision": "218314665e537b02e2353265323f2d45",
+    "url": "/testdeploy/index.html"
+  }
+];
